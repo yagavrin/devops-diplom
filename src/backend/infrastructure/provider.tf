0c772f83cbe866545eb80cdef715eb6cfcddd2c9
Yuriy <y.gavrinskiy@mail.ru>
2025-08-04 01:31:52 +0300

    add provider

---
+++ src/backend/infrastructure/provider.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+  required_version = ">=1.8.4"
+
+  backend "s3" {
+    endpoints = {
+      s3 = "https://storage.yandexcloud.net"
+    }
+    bucket = "yagavrin-tf-backend"
+    region = "ru-central1-d"
+    key = "terraform.tfstate"
+
+    skip_region_validation = true
+    skip_credentials_validation = true
+
+    skip_requesting_account_id = true
+    skip_s3_checksum = true
+  }
+}
+
+provider "yandex" {
+  token     = var.token
+  cloud_id  = var.cloud_id
+  folder_id = var.folder_id
+  zone      = var.default_zone
+}
