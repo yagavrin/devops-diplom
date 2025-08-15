afbc98289d76aecc40e21df441f295cdddfe766b
Yuriy <y.gavrinskiy@mail.ru>
2025-08-15 21:29:53 +0300

    update provider

--- src/backend/infrastructure/provider.tf
+++ src/backend/infrastructure/provider.tf
@@ -18,6 +18,10 @@ terraform {
     use_path_style       = true
 
     skip_region_validation = true
+    skip_credentials_validation = true
+
+    skip_requesting_account_id = true
+    skip_s3_checksum = true
   }
 }
 
