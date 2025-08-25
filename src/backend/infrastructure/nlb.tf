8360368c9fd2d408608cff1e5211146cf9bd5193
Yuriy <y.gavrinskiy@mail.ru>
2025-08-26 00:12:40 +0300

    add ycr service account

--- src/backend/infrastructure/nlb.tf
+++ src/backend/infrastructure/nlb.tf
@@ -1,37 +1,37 @@
-resource "yandex_lb_target_group" "k8s_ingress_targets" {
-  name      = "k8s-target-group"
+# resource "yandex_lb_target_group" "k8s_ingress_targets" {
+#   name      = "k8s-target-group"
 
-  dynamic "target" {
-    for_each = concat(
-      yandex_compute_instance.k8s_cp_group[*],
-      yandex_compute_instance.k8s_worker_group[*]
-    )
-    content {
-      subnet_id = target.value.network_interface.0.subnet_id
-      address   = target.value.network_interface.0.ip_address
-    }
-  }
-}
+#   dynamic "target" {
+#     for_each = concat(
+#       yandex_compute_instance.k8s_cp_group[*],
+#       yandex_compute_instance.k8s_worker_group[*]
+#     )
+#     content {
+#       subnet_id = target.value.network_interface.0.subnet_id
+#       address   = target.value.network_interface.0.ip_address
+#     }
+#   }
+# }
 
-resource "yandex_lb_network_load_balancer" "k8s_ingress_lb" {
-  name               = "k8s-ingress-lb"
-  type               = "external"
-  listener {
-    name = "http"
-    port = 80
-    external_address_spec {
-      ip_version = "ipv4"
-    }
-  }
+# resource "yandex_lb_network_load_balancer" "k8s_ingress_lb" {
+#   name               = "k8s-ingress-lb"
+#   type               = "external"
+#   listener {
+#     name = "http"
+#     port = 80
+#     external_address_spec {
+#       ip_version = "ipv4"
+#     }
+#   }
 
-  attached_target_group {
-    target_group_id = yandex_lb_target_group.k8s_ingress_targets.id
-    healthcheck {
-      name = "http"
-      http_options {
-        port = 80
-        path = "/"
-      }
-    }
-  }
-}
+#   attached_target_group {
+#     target_group_id = yandex_lb_target_group.k8s_ingress_targets.id
+#     healthcheck {
+#       name = "http"
+#       http_options {
+#         port = 80
+#         path = "/"
+#       }
+#     }
+#   }
+# }
--- src/backend/infrastructure/registry.tf
+++ src/backend/infrastructure/registry.tf
@@ -6,6 +6,22 @@ resource "yandex_container_registry" "container_registry" {
   }
 }
 
+resource "yandex_iam_service_account" "ycr_sa" {
+  name        = "ycr-sa"
+  description = "Service account for Yandex Container Registry"
+}
+
+resource "yandex_resourcemanager_folder_iam_member" "ycr_pusher" {
+  folder_id = var.folder_id
+  role      = "container-registry.images.pusher"
+  member    = "serviceAccount:${yandex_iam_service_account.ycr_sa.id}"
+}
+
+
 output "registry_id" {
   value = yandex_container_registry.container_registry.id
+}
+
+output "ycr_sa_id" {
+  value = yandex_iam_service_account.ycr_sa.id
 }
